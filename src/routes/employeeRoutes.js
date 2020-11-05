d778a47489bd9d2fb4a4dfba650e1fb63b74e545
Ruzgar Ata Ozkan <ruzgarata6@gmail.com>
2020-11-05 21:22:23 +0300

    employee/delete route has been added

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -12,6 +12,12 @@ const router = express.Router();
 
 router.use(authentication);
 
+router.get('/employees', async (req, res) => {
+    const userId = req.user._id;
+    const employees = await Employee.find({ userId });
+    res.send(employees);
+});
+
 router.post('/employees', async (req, res) => {
     const { name, phone, shift } = req.body;
     const userId = req.user._id;
@@ -24,15 +30,17 @@ router.post('/employees', async (req, res) => {
 router.post('/employees/edit', async (req, res) => {
     const { name, phone, shift, employeeId } = req.body;
     await Employee.updateOne({ _id: employeeId }, { $set: { name, phone, shift } });
-    //const employee = await Employee.findOne({ _id: employeeId });
-    //await employee.save();
     res.send({ success: 'true', msg: 'Employee has been edited' });
 });
 
-router.get('/employees', async (req, res) => {
-    const userId = req.user._id;
-    const employees = await Employee.find({ userId });
-    res.send(employees);
-});
+router.post('/employees/delete', async (req, res) => {
+    const { employeeId } = req.body;
+    Employee.deleteOne({ _id: employeeId }, (err) => {
+        if (err) {
+            return res.status(422).send({ error: 'delete-employe', msg: 'Error while deleting the employee' });
+        }
+    });
+    res.send({ success: true });
+})
 
 module.exports = router;
